6c810bc01d5a92415889cdb5a151cae0a7817b20
Tan Jun Hong <junhoongtan@gmail.com>
2022-05-16 20:26:43 +0800

    add listener boilerplate

--- lib/features/splash_screen/cubit/theme_cubit.dart
+++ lib/features/splash_screen/cubit/theme_cubit.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:equatable/equatable.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:json_annotation/json_annotation.dart';
@@ -16,6 +18,24 @@ class ThemeCubit extends HydratedCubit<ThemeState> {
   Map<String, dynamic> toJson(state) => state.toJson();
 
   Future<void> fetchTheme() async {
+    sleep(Duration(seconds: 5));
+    print(state);
     emit(state.copyWith(appTheme: AppTheme.dark, status: ThemeStatus.success));
+    // print(state);
+    // sleep(Duration(seconds: 2));
+    // emit(state.copyWith(appTheme: AppTheme.light, status: ThemeStatus.initial));
+    // print(state);
+    // sleep(Duration(seconds: 2));
+    // emit(state.copyWith(appTheme: AppTheme.light, status: ThemeStatus.success));
+    // print(state);
+    // // print(state);
+    // emit(state.copyWith(appTheme: AppTheme.light, status: ThemeStatus.success));
+    // print(state);
+  }
+
+  @override
+  void onChange(Change<ThemeState> change) {
+    super.onChange(change);
+    print(change);
   }
 }
--- lib/features/splash_screen/views/splash_screen.dart
+++ lib/features/splash_screen/views/splash_screen.dart
@@ -1,7 +1,10 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:fluro/fluro.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:gamaverse/features/splash_screen/cubit/theme_cubit.dart';
+import 'package:gamaverse/utils/routers/fluro_application.dart';
+import 'package:gamaverse/utils/routers/router.dart';
 import 'package:gamaverse/utils/theme/theme.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -16,17 +19,49 @@ class SplashScreen extends StatelessWidget {
   }
 }
 
-class SplashScreenView extends StatelessWidget {
+class SplashScreenView extends StatefulWidget {
   const SplashScreenView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    context.read<ThemeCubit>().fetchTheme();
+  State<SplashScreenView> createState() => _SplashScreenViewState();
+}
 
+class _SplashScreenViewState extends State<SplashScreenView> {
+  _SplashScreenViewState() {
+    final router = FluroRouter();
+    Routers.configureRoutes(router);
+    FluroApplication.router = router;
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => context.read<ThemeCubit>().fetchTheme());
+    // print('asdf');
+    // print(context.read<ThemeCubit>().state);
+    // context.read<ThemeCubit>().fetchTheme();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return BlocConsumer<ThemeCubit, ThemeState>(
       listener: (context, state) {
+        print(state);
         if (state.status.isSuccess) {
-          //TODO: navigate
+          // print('asdf');
+          // ScaffoldMessenger.of(context)
+          //     .showSnackBar(SnackBar(content: Text(state.appTheme.name)));
+          print('qwerqwer');
+
+          // FluroApplication.router.navigateTo(
+          //   context,
+          //   'comic_reader',
+          // );
+        } else {
+          print('zxcv');
+          // ScaffoldMessenger.of(context)
+          //     .showSnackBar(SnackBar(content: Text(state.appTheme.name)));
         }
       },
       builder: (context, state) {
@@ -40,7 +75,8 @@ class SplashScreenView extends StatelessWidget {
           supportedLocales: context.supportedLocales,
           locale: context.locale,
           theme: appThemeData[state.appTheme],
-          home: const Scaffold(
+          onGenerateRoute: FluroApplication.router.generator,
+          home: Scaffold(
             backgroundColor: Colors.black,
             body: Center(
                 child: Image(image: AssetImage('assets/images/logo.png'))),
